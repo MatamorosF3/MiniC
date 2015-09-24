2191b61e2233da886f0785c6e3f8a2febbe9304a
iamsanchez <sedsanchez93@gmail.com>
2015-09-24 01:10:51 -0600

    CUP2 & AST

---
+++ src/AST/factor_int.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AST;
+
+/**
+ *
+ * @author C5220696
+ */
+public class factor_int {
+
+    
+    int a;
+    public factor_int(int a ) {
+    this.a = a;
+        System.out.println("sjdflkjslkfsjfldsjflksjdflksjflksjfdlkjdslkfjslkfslfjslkfjs");
+    }
+    
+    
+    
+}
---
+++ src/AST/literal_int.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AST;
+
+/**
+ *
+ * @author C5220696
+ */
+public class literal_int {
+    
+    
+    String a;
+    public literal_int(String a){
+        this.a = a;
+        System.out.println("AAAA");
+    }
+    
+    
+    
+}
